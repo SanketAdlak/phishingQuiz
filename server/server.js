8323e67d61f39b04f44378c0f61bb55ec970068c
Sanket Adlak <sanketadlak@outlook.com>
2025-11-16 00:00:46 +0530

    fix: single server deployment; serve static build

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Serve static files from the React app
-//app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 const resultsFilePath = path.join(__dirname, 'results.json');
 const csvFilePath = path.join(__dirname, 'results.csv');
@@ -244,9 +244,9 @@ app.get('/api/question2/:id', (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
